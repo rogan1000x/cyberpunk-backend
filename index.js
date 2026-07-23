7f78565bb5813b935993f352f5338d1e2dc2ea48
Rogan Kim <rogan1000x@gmail.com>
2026-07-23 16:26:13 +0900

    feat: MongoDB Atlas 연동 (mongoose)

--- index.js
+++ index.js
@@ -1,61 +1,52 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');  
+const mongoose = require('mongoose');
+const Project = require('./models/Project');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-const DATA_FILE = './projects.json';
-
-// 파일에서 데이터 읽기
-function readProjects() {
-  const data = fs.readFileSync(DATA_FILE, 'utf-8');
-  return JSON.parse(data);
-}
-
-// 파일에 데이터 쓰기
-function saveProjects(projects) {
-  fs.writeFileSync(DATA_FILE, JSON.stringify(projects, null, 2));
-}
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB 연결 성공!'))
+  .catch((err) => console.log('MongoDB 연결 실패:', err));
 
 app.get('/', (req, res) => {
   res.send('안녕하세요! 서버가 작동 중입니다!');
 });
 
-app.get('/api/projects', (req, res) => {
-  const projects = readProjects();
+// 전체 조회
+app.get('/api/projects', async (req, res) => {
+  const projects = await Project.find();
   res.json(projects);
 });
 
-app.post('/api/projects', (req, res) => {
-  const projects = readProjects();
-  const newProject = req.body;
-  projects.push(newProject);
-  saveProjects(projects);
+// 추가
+app.post('/api/projects', async (req, res) => {
+  const newProject = new Project(req.body);
+  await newProject.save();
+  const projects = await Project.find();
   res.json({ message: '프로젝트가 추가되었습니다!', projects });
 });
 
-app.delete('/api/projects/:index', (req, res) => {
-  const projects = readProjects();
-  const indexToDelete = parseInt(req.params.index);
-  
-  projects.splice(indexToDelete, 1);
-  saveProjects(projects);
-  
-  res.json({ message: '프로젝트가 삭제되었습니다!', projects });
+// 수정
+app.put('/api/projects/:id', async (req, res) => {
+  await Project.findByIdAndUpdate(req.params.id, req.body);
+  const projects = await Project.find();
+  res.json({ message: '프로젝트가 수정되었습니다!', projects });
 });
 
-app.put('/api/projects/:index', (req, res) => {
-  const projects = readProjects();
-  const indexToUpdate = parseInt(req.params.index);
-  const updatedProject = req.body;
-
-  projects[indexToUpdate] = updatedProject;
-  saveProjects(projects);
-
-  res.json({ message: '프로젝트가 수정되었습니다!', projects });
+// 삭제
+app.delete('/api/projects/:id', async (req, res) => {
+  await Project.findByIdAndDelete(req.params.id);
+  const projects = await Project.find();
+  res.json({ message: '프로젝트가 삭제되었습니다!', projects });
 });
 
 app.listen(PORT, () => {
